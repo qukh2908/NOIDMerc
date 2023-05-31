5ede662f41fba973f7ac8f287e7d3b1a1235a9d9
KhanhTran <kylekhanh1028@gmail.com>
2023-05-31 19:55:16 +0700

    Test commit 4

--- app/src/main/java/com/example/noidmerchant/MainActivity.java
+++ app/src/main/java/com/example/noidmerchant/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         
         //KHANH CHAO CA NHA IU
+        //THIS IS MAIN BRANCHES
     }
 }
