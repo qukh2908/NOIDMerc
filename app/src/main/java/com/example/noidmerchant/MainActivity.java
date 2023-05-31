37520db5bda98d205c0f84bedd30ac937efd28f4
KhanhTran <kylekhanh1028@gmail.com>
2023-05-31 20:03:15 +0700

    Test commit 6

--- app/src/main/java/com/example/noidmerchant/MainActivity.java
+++ app/src/main/java/com/example/noidmerchant/MainActivity.java
@@ -10,12 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        //KHANH CHAO CA NHA IU
-        //THIS IS MY MAIN BRANCHES
 
+        //CHAO CA NHA IU
         //THIS IS MAIN BRANCHES
-
-        //HHAHAA
     }
 }
