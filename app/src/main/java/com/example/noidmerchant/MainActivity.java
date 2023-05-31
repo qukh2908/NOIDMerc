945f1affa2188cf644b95af6edba9f166db15b97
KhanhTran <kylekhanh1028@gmail.com>
2023-05-31 19:27:44 +0700

    Initial commit

---
+++ app/src/main/java/com/example/noidmerchant/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.noidmerchant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //KHANH CHAO CA NHA IU
+    }
+}
