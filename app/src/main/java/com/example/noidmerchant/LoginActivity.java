a571de2d864f16806266cf49c354d11ce2fad5b5
Kuroky <optimusss247@gmail.com>
2023-06-01 15:43:21 +0700

    Duc Anh 1/6/2023 - fig 2

--- app/src/main/java/com/example/noidmerchant/LoginActivity.java
+++ app/src/main/java/com/example/noidmerchant/LoginActivity.java
@@ -1,16 +1,127 @@
 package com.example.noidmerchant;
 
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
+    // tao bien Authentication
+    private FirebaseAuth auth;
+    //Tao bien login email va password
+    private TextInputEditText logEmail,logPassword;
+    //Tao bien nut dang nhap
+    private Button loginButton;
+    //Tao bien cho Dang ky va quen mat khau
+    private TextView signupRedirectText,forgotPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        auth = FirebaseAuth.getInstance();
+        logEmail = findViewById(R.id.loginEmail);
+        logPassword = findViewById(R.id.loginPassword);
+        loginButton = findViewById(R.id.btnSignIn);
+        signupRedirectText = findViewById(R.id.txtRegAccount);
+        forgotPassword = findViewById(R.id.txtForgotPassword);
 
-        //CHAO CA NHA IU
-        //THIS IS MAIN BRANCHES
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = logEmail.getText().toString();
+                String pass = logPassword.getText().toString();
+
+                if(!email.isEmpty()&& Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    if (!pass.isEmpty()){
+                        auth.signInWithEmailAndPassword(email,pass)
+                                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                    @Override
+                                    public void onSuccess(AuthResult authResult) {
+                                        Toast.makeText(LoginActivity.this,"Dang Nhap Thanh cong",Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                        finish();
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(LoginActivity.this,"Sai mat khau hoac email vui long nhap lai",Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                    }else{
+                        logPassword.setError("Empty fields are not allowed");
+                    }
+                }else if (email.isEmpty()){
+                    logEmail.setError("Empty fields are not allowed");
+                }else{
+                    logEmail.setError("Please enter correct email");
+                }
+            }
+        });
+        signupRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                View dialogView = getLayoutInflater().inflate(R.layout.dialog_forgotpassword, null);
+                TextInputEditText emailBox = dialogView.findViewById(R.id.edtForgotEmail);
+                builder.setView(dialogView);
+                AlertDialog dialog = builder.create();
+                dialogView.findViewById(R.id.btnConfirm).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String userEmail = emailBox.getText().toString();
+                        if(TextUtils.isEmpty(userEmail)&&!Patterns.EMAIL_ADDRESS.matcher(userEmail).matches()){
+                            Toast.makeText(LoginActivity.this, "Enter your registered email id", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        auth.sendPasswordResetEmail(userEmail).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()){
+                                    Toast.makeText(LoginActivity.this, "Check your email", Toast.LENGTH_SHORT).show();
+                                    dialog.dismiss();
+                                } else {
+                                    Toast.makeText(LoginActivity.this, "Unable to send, failed", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    }
+                });
+                dialogView.findViewById(R.id.btnCancel).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+                if(dialog.getWindow()!=null)
+                {
+                    dialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+                }
+                dialog.show();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/noidmerchant/RegisterActivity.java
+++ app/src/main/java/com/example/noidmerchant/RegisterActivity.java
@@ -1,13 +1,80 @@
 package com.example.noidmerchant;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class RegisterActivity extends AppCompatActivity {
+    //tao bien  Firebase Authentication
+    private FirebaseAuth auth;
+    //tao bien cho edittext cua email va password
+    private TextInputEditText signupEmail,signupPassword;
+    //bien nut dang ki
+    private Button btnSignup;
+    //bien quen mat khau
+    private TextView loginRedirectText;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        // Tim den dung duong dan cac id
+        auth = FirebaseAuth.getInstance();
+        signupEmail = findViewById(R.id.edtEmail);
+        signupPassword = findViewById(R.id.edtPassword);
+        btnSignup = findViewById(R.id.btnSignUp);
+        loginRedirectText = findViewById(R.id.txtSignIn);
+        //Bat su kien cho dang ki
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // gan user va password
+                String user = signupEmail.getText().toString().trim();
+                String password = signupPassword.getText().toString().trim();
+                //Xet dieu kien cho user va password
+                if(user.isEmpty()){
+                    signupEmail.setError("Email khong duoc de trong");
+                }
+                if(password.isEmpty())
+                {
+                    signupPassword.setError("Mat khau khong duoc de trong");
+                }else {
+                    //Bat su kien cho Auth
+                    auth.createUserWithEmailAndPassword(user,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            //Xet dieu kien cho email va mat khau
+                            if(task.isSuccessful()){
+                                Toast.makeText(RegisterActivity.this,"Dang ky thanh cong",Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+
+                            }else{
+                                Toast.makeText(RegisterActivity.this,"Mat Khau hoac email bi sai"+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+        loginRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+            }
+        });
     }
 }
