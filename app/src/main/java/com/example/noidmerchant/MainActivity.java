aec5fa60dbad557ff082a0ce398e883bea1f77a5
KhanhTran <kylekhanh1028@gmail.com>
2023-06-01 10:28:20 +0700

    ADDING REALTIME DATABASE

--- app/src/main/java/com/example/noidmerchant/MainActivity.java
+++ app/src/main/java/com/example/noidmerchant/MainActivity.java
@@ -4,13 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+public class MainActivity extends AppCompatActivity {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //CHAO CA NHA
 
-        //CHAO CA NHA IUuu
+        //KHANH TEST REALTIMEDB
+        myRef.setValue("Hello, World!");
     }
 }
