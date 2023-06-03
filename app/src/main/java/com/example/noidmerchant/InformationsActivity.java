9399727ae6932a17e4e5ca139a50c4b3923527e4
KhanhTran <kylekhanh1028@gmail.com>
2023-06-03 15:47:56 +0700

    KHANH adding backbtn for InformationsActivity

--- app/src/main/java/com/example/noidmerchant/InformationsActivity.java
+++ app/src/main/java/com/example/noidmerchant/InformationsActivity.java
@@ -3,12 +3,19 @@ package com.example.noidmerchant;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class InformationsActivity extends AppCompatActivity {
 
+    ImageView back_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_informations);
+        back_btn = findViewById(R.id.back_btn);
+
+        back_btn.setOnClickListener(v -> {
+            finish();
+        });
     }
 }
