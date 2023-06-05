7d7c0bff0c7cf0ef1c01d8e8e16663e54892ca7e
KhanhTran <kylekhanh1028@gmail.com>
2023-06-05 11:27:03 +0700

    KHANH organize folder

--- app/src/main/java/com/example/noidmerchant/gui/InformationsActivity.java
+++ app/src/main/java/com/example/noidmerchant/gui/InformationsActivity.java
@@ -1,19 +1,15 @@
 package com.example.noidmerchant.gui;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.noidmerchant.R;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -42,13 +38,8 @@ public class InformationsActivity extends AppCompatActivity {
         btnSave = findViewById(R.id.btn_luu);
         database = FirebaseDatabase.getInstance();
         FirebaseUser userUID =  FirebaseAuth.getInstance().getCurrentUser();
+        assert userUID != null;
         uid = userUID.getUid();
-
-        if(uid != null) {
-            Log.d("ADebugTag", "UID: " + uid);
-        } else {
-            Log.d("ADebugTag", "UID IS NULL ");
-        }
 
         refEmail = database.getReference(path + uid + "/email");
         refName = database.getReference(path + uid + "/name");
@@ -106,32 +97,18 @@ public class InformationsActivity extends AppCompatActivity {
             }
         });
 
-
-        back_btn.setOnClickListener(v -> {
-            finish();
-        });
-        btnSave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        back_btn.setOnClickListener(v -> finish());
+        btnSave.setOnClickListener(v -> {
 //
-                refDb.child(path).setValue(uid)
-                        .addOnSuccessListener(new OnSuccessListener<Void>() {
-                            @Override
-                            public void onSuccess(Void unused) {
-                                refEmail.setValue(txtEmail.getText().toString());
-                                refAdd.setValue(txtAdd.getText().toString());
-                                refName.setValue(txtName.getText().toString());
-                                refPhone.setValue(txtPhone.getText().toString());
-                                Toast.makeText(InformationsActivity.this, "Lưu thành công", Toast.LENGTH_SHORT).show();
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Toast.makeText(InformationsActivity.this, "Không thành công", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-            }
+            refDb.child(path).setValue(uid)
+                    .addOnSuccessListener(unused -> {
+                        refEmail.setValue(txtEmail.getText().toString());
+                        refAdd.setValue(txtAdd.getText().toString());
+                        refName.setValue(txtName.getText().toString());
+                        refPhone.setValue(txtPhone.getText().toString());
+                        Toast.makeText(InformationsActivity.this, "Lưu thành công", Toast.LENGTH_SHORT).show();
+                    })
+                    .addOnFailureListener(e -> Toast.makeText(InformationsActivity.this, "Không thành công", Toast.LENGTH_SHORT).show());
         });
     }
 }
