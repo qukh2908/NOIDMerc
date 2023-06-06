ba0bb92f29ba86fd67872fd1bdc191c29ff68ef3
MyApp <giatien2503@gmail.com>
2023-06-06 12:40:35 +0700

    Thêm sự kiện nút back

--- app/src/main/java/com/example/noidmerchant/gui/OrdersActivity.java
+++ app/src/main/java/com/example/noidmerchant/gui/OrdersActivity.java
@@ -3,14 +3,20 @@ package com.example.noidmerchant.gui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.example.noidmerchant.R;
 
 public class OrdersActivity extends AppCompatActivity {
 
+    ImageView back_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_orders);
+        back_btn = findViewById(R.id.back_btn);
+
+        //nút back
+        back_btn.setOnClickListener(v -> finish());
     }
 }
--- app/src/main/java/com/example/noidmerchant/gui/ProductsActivity.java
+++ app/src/main/java/com/example/noidmerchant/gui/ProductsActivity.java
@@ -5,6 +5,7 @@ import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.example.noidmerchant.R;
 import com.example.noidmerchant.adapter.ViewPagerAdapter;
@@ -16,6 +17,7 @@ import com.google.firebase.database.FirebaseDatabase;
 public class ProductsActivity extends AppCompatActivity {
     private TabLayout mTab;
     private ViewPager mView;
+    ImageView back_btn;
 
 
     final FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -26,6 +28,7 @@ public class ProductsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_products);
         mTab = findViewById(R.id.tab_layout);
         mView = findViewById(R.id.view_pager);
+        back_btn = findViewById(R.id.back_btn);
         ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
         mView.setAdapter(viewPagerAdapter);
         mTab.setupWithViewPager(mView);
@@ -33,5 +36,7 @@ public class ProductsActivity extends AppCompatActivity {
         //Thêm danh mục lên realtime DB
         //cateRef.push().setValue(new DBCategory("Coffee"));
         //Thêm danh mục lên realtime DB
+        //nút back
+        back_btn.setOnClickListener(v -> finish());
     }
 }
