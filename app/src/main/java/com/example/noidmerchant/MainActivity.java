c424ba0cccd47935ab061c0de18e594b4e1c19e5
KhanhTran <kylekhanh1028@gmail.com>
2023-06-06 14:45:27 +0700

    KHANH chỉnh vài thứ

--- app/src/main/java/com/example/noidmerchant/MainActivity.java
+++ app/src/main/java/com/example/noidmerchant/MainActivity.java
@@ -2,26 +2,24 @@ package com.example.noidmerchant;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
-
 import com.example.noidmerchant.GUI.Common.InformationsActivity;
+import com.example.noidmerchant.GUI.Common.SettingsActivity;
 import com.example.noidmerchant.GUI.Imports.BuyProductActivity;
 import com.example.noidmerchant.GUI.Orders.OrdersActivity;
 import com.example.noidmerchant.GUI.Products.ProductsActivity;
-import com.example.noidmerchant.GUI.Common.SettingsActivity;
 import com.example.noidmerchant.GUI.Statistics.StatisticsActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private CardView products,orders,statistics,helps,info,settings;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CardView products,orders,statistics,helps,info,settings;
+
         products = findViewById(R.id.productCard);
         orders = findViewById(R.id.ordersCard);
         statistics = findViewById(R.id.statisticsCard);
@@ -30,43 +28,16 @@ public class MainActivity extends AppCompatActivity {
         settings = findViewById(R.id.settingsCard);
 
         //Bat su kien Product
-        products.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, ProductsActivity.class));
-            }
-        });
+        products.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, ProductsActivity.class)));
         //Bat su kien Order
-        orders.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, OrdersActivity.class));
-            }
-        });
+        orders.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, OrdersActivity.class)));
 
-        statistics.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, StatisticsActivity.class));
-            }
-        });
-        helps.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, BuyProductActivity.class));
-            }
-        });
-        info.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, InformationsActivity.class));
-            }
-        });
-        settings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, SettingsActivity.class));
-            }
-        });
+        statistics.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, StatisticsActivity.class)));
+
+        helps.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, BuyProductActivity.class)));
+
+        info.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, InformationsActivity.class)));
+
+        settings.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, SettingsActivity.class)));
     }
 }
