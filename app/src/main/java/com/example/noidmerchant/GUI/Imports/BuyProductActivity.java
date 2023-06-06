a381ce63d96f870e3fb8470a9dc22b95ddd26625
KhanhTran <kylekhanh1028@gmail.com>
2023-06-06 20:34:47 +0700

    KHANH daily commit

--- app/src/main/java/com/example/noidmerchant/GUI/Imports/BuyProductActivity.java
+++ app/src/main/java/com/example/noidmerchant/GUI/Imports/BuyProductActivity.java
@@ -6,25 +6,34 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.ImageView;
+import android.widget.TableRow;
 
 import com.example.noidmerchant.R;
+
+import java.util.ArrayList;
 
 public class BuyProductActivity extends AppCompatActivity {
 
     AutoCompleteTextView edt_danhmuc;
+    ArrayAdapter<String> dmAdapter;
     ImageView back_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buyproduct);
+
         edt_danhmuc = findViewById(R.id.edt_danhmuc);
         back_btn = findViewById(R.id.back_btn);
 
-        String[] drinks = {"Cà phê", "Nước ngọt", "Sinh tố"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>
-                (this, android.R.layout.select_dialog_item, drinks);
-        edt_danhmuc.setAdapter(adapter);//setting the adapter data into the AutoCompleteTextView
+        ArrayList<String> drinks = new ArrayList<String>();
+        drinks.add("ABC");
+        drinks.add("123");
 
+        edt_danhmuc.setOnClickListener(v-> {
+            dmAdapter = new ArrayAdapter<> (this, android.R.layout.simple_dropdown_item_1line, drinks);
+            edt_danhmuc.setAdapter(dmAdapter);
+            edt_danhmuc.showDropDown();
+        });
 
         //nút back
         back_btn.setOnClickListener(v -> finish());
