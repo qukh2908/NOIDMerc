fe1bfbba1115a278628aa7b672504571819e19d2
KhanhTran <kylekhanh1028@gmail.com>
2023-06-09 12:41:07 +0700

    KHANH hoàn thiện thêm sửa xóa xuatban

--- app/src/main/java/com/example/noidmerchant/GUI/Orders/OrdersActivity.java
+++ app/src/main/java/com/example/noidmerchant/GUI/Orders/OrdersActivity.java
@@ -1,9 +1,11 @@
 package com.example.noidmerchant.GUI.Orders;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -12,23 +14,25 @@ import com.example.noidmerchant.Database.DBProduct;
 import com.example.noidmerchant.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 public class OrdersActivity extends AppCompatActivity {
     final FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference prodRef = database.getReference("sanpham");
-    DatabaseReference expRef = database.getReference().child("xuatban");
-    DatabaseReference ordRef = database.getReference().child("donhangmua");
-    ImageView back_btn;
-    FirebaseUser userUID = FirebaseAuth.getInstance().getCurrentUser();
-    String uid= userUID.getUid(); //Lấy mã khách hàng hiện đang login
-
-//    String masp = "-NXP2TOjFqGV2o1xzY3l"; //masp tùy khách chọn sp nào thì getkey sp đó gán vào
-//    int soluongxb = 2; //số lợng tùy khách nhập
+    private FirebaseUser userUID = FirebaseAuth.getInstance().getCurrentUser();
+    private String uid = userUID.getUid(); //Lấy mã khách hàng hiện đang login
+    private DatabaseReference prodRef = database.getReference("sanpham");
+    private DatabaseReference expRef = database.getReference().child("xuatban");
+    private DatabaseReference ordRef = database.getReference().child("donhangmua");
+    private ImageView back_btn;
+    private String masp = "-NXP2U9qvuzfN4bCUk7Z"; //masp tùy khách chọn sp nào thì getkey sp đó gán vào
+    private String maxb; //mã xb tự sinh tự gán mỗi khi tạo
+    private int soluongxb = 3; //số lợng tùy khách nhập
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,13 +41,45 @@ public class OrdersActivity extends AppCompatActivity {
         back_btn = findViewById(R.id.back_btn);
 
         //Thêm xuất bán lên realtime DB
-//        prodRef.child(masp).child("giasp").addListenerForSingleValueEvent(new ValueEventListener() {
+//        prodRef.child(masp).limitToFirst(1).addChildEventListener(new ChildEventListener() {
 //            @Override
-//            public void onDataChange(@NonNull DataSnapshot snapshot) {
-//                int giasp = snapshot.getValue(int.class);
-//                int giaxp = giasp * soluongxb;
-//                expRef.push().setValue(new DBExport(uid, masp, soluongxb, giaxp, " "));
+//            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+//                Object giasp = snapshot.getValue();
+//                int giaxb = giasp.hashCode() * soluongxb;
+//                maxb = expRef.push().getKey();
+//                expRef.child(maxb).setValue(new DBExport(uid, masp, soluongxb, giaxb, " "));
 //            }
+//
+//            @Override
+//            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+//                Object giasp = snapshot.getValue();
+//                int giaxb = giasp.hashCode() * soluongxb;
+//                expRef.child(maxb).child("giaxb").setValue(giaxb);
+//            }
+//
+//            @Override
+//            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+//                expRef.child(maxb).child("madhm").addListenerForSingleValueEvent(new ValueEventListener() {
+//                    @Override
+//                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                        Object madhm = snapshot.getValue();
+//                        if(madhm.toString().trim().isEmpty()) {
+//                            expRef.child(maxb).removeValue();
+//                        }
+//                    }
+//
+//                    @Override
+//                    public void onCancelled(@NonNull DatabaseError error) {
+//                        //
+//                    }
+//                });
+//            }
+//
+//            @Override
+//            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+//                //
+//            }
+//
 //            @Override
 //            public void onCancelled(@NonNull DatabaseError error) {
 //                //
