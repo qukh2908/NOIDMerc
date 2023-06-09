4ec324d0d2c38f1e4e0f77e13d015be99cee7f1d
KhanhTran <kylekhanh1028@gmail.com>
2023-06-09 14:46:07 +0700

    KHANH làm nút logout

--- app/src/main/java/com/example/noidmerchant/GUI/Common/SettingsActivity.java
+++ app/src/main/java/com/example/noidmerchant/GUI/Common/SettingsActivity.java
@@ -1,20 +1,46 @@
 package com.example.noidmerchant.GUI.Common;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.noidmerchant.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SettingsActivity extends AppCompatActivity {
-
-    ImageView back_btn;
+    private DialogInterface.OnClickListener dialogClickListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        back_btn = findViewById(R.id.back_btn);
+        ImageView back_btn = findViewById(R.id.back_btn);
+        TextView tv_logout = findViewById(R.id.tv_logout);
+
+        //Nút đăng xuất
+        tv_logout.setOnClickListener(v-> {
+            dialogClickListener = (dialog, which) -> {
+                switch (which) {
+                    case DialogInterface.BUTTON_POSITIVE:
+                        FirebaseAuth.getInstance().signOut();
+                        startActivity(new Intent(SettingsActivity.this, LoginActivity.class));
+                        finish();
+                        break;
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        dialog.dismiss();
+                }
+            };
+            AlertDialog.Builder builder = new AlertDialog.Builder(SettingsActivity.this);
+            builder.setMessage("Bạn có chắc muốn rời bỏ tôi?")
+                    .setPositiveButton("Có", dialogClickListener)
+                    .setNegativeButton("Không", dialogClickListener)
+                    .show();
+        });
 
         //nút back
         back_btn.setOnClickListener(v -> finish());
